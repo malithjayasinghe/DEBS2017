e0a6e13170f8ddefe852959815b55660e5b800eb
nihlaakram <nihlaakram@yahoo.com>
2017-01-30 10:49:57 +0530

    KMeans clustering for one dimension and test cases

--- src/main/java/org/wso2/siddhi/debs2017/kmeans/WSO2KmeansClustering.java
+++ src/main/java/org/wso2/siddhi/debs2017/kmeans/WSO2KmeansClustering.java
@@ -1,10 +1,161 @@
 package org.wso2.siddhi.debs2017.kmeans;
 
-/**
- * Created by temp on 1/25/17.
- */
+import java.util.ArrayList;
+
+/*
+* Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 public class WSO2KmeansClustering {
 
+
+
+    private int k;
+    private int maxIter;
+    private ArrayList<Double> data = new ArrayList<>();
+    private ArrayList<ArrayList<Double>> clusterGroup;
+    private ArrayList<Double> center;
+    private ArrayList<Double> centerOld;
+
+    public WSO2KmeansClustering(int c, int max, ArrayList<Double> dataSet ){
+
+        //intialize variables
+        this.k = c;
+        this.maxIter = max;
+        this.data = dataSet;
+        this.clusterGroup = new ArrayList<>();
+        this.center = new ArrayList<>();
+        this.centerOld = new ArrayList<>();
+
+        //create a group to hold data related to each cluster
+        createGroup();
+        //System.out.println("No of Clusters :"+this.k+"\nNo of clustergroups :"+this.clusterGroup.size());
+
+        //initialize the cluster centers to first k values
+        initializeCenters();
+        /*System.out.println("\nInitial center values");
+        for (int i=0; i<center.size(); i++){
+            System.out.println("c"+i+":"+center.get(i));
+        }*/
+
+        int iter = 0;
+        do {
+            //calculate the nearest center to each data point and add the data to the cluster of respective center
+            assignToCluster();
+            /*System.out.println("\nChecking each cluster");
+            for (int i=0; i<clusterGroup.size(); i++){
+                System.out.println("clusterG "+i+":"+clusterGroup.get(i));
+            };
+
+            for (int i = 0; i < center.size(); i++) {
+                System.out.println("New C" + (i) + " " + center.get(i));
+            }*/
+
+
+            //reinitialize the cluster centres and store the old ones
+            reintializeCluster(iter);
+
+            if (!center.equals(centerOld)) {
+                for (int i = 0; i < clusterGroup.size(); i++) {
+                    clusterGroup.get(i).removeAll(clusterGroup.get(i));
+                }
+            }
+            iter++;
+        } while (!center.equals(centerOld) && iter<=maxIter);
+
+        //System.out.println("Iterations : "+iter);
+
+
+    }
+
+    private void reintializeCluster(int iter) {
+        for (int i = 0; i < k; i++) {
+            if (iter == 0) {
+                centerOld.add(center.get(i));
+            } else {
+                centerOld.set(i, center.get(i));
+            }
+            if (!clusterGroup.get(i).isEmpty()) {
+                center.set(i, average(clusterGroup.get(i)));
+            }
+        }
+    }
+
+    private Double average(ArrayList<Double> doubles) {
+        double sum = 0;
+        for (int i =0; i<doubles.size(); i++) {
+            sum = sum +doubles.get(i);
+        }
+        return (sum/doubles.size());
+    }
+
+    private void assignToCluster() {
+        ArrayList<Double> difference = new ArrayList<>();
+        double dataItem, cenVal, diff;
+        for(int i =0; i<data.size(); i++) {
+            dataItem = data.get(i);
+            for (int j=0; j<center.size(); j++) {
+                cenVal = center.get(j);
+                diff = Math.abs(cenVal-dataItem);
+                difference.add(diff);
+
+            }
+            int minIndex = getMinIndex(difference);
+            clusterGroup.get(minIndex).add(dataItem);
+            difference.removeAll(difference);
+        }
+    }
+
+    private void initializeCenters() {
+        for (int i=0; i<this.k; i++) {
+            center.add(data.get(i));
+        }
+    }
+
+    private void createGroup() {
+        for(int i=0; i<this.k; i++){
+            this.clusterGroup.add(new ArrayList<>());
+        }
+
+    }
+
+
+    private int getMinIndex(ArrayList<Double> diff) {
+        int minIndex = 0;
+        for(int i = 1; i<diff.size(); i++){
+            if(diff.get(minIndex)>diff.get(i)){
+                minIndex = i;
+            }
+
+        }
+        // System.out.println(diff);
+        return minIndex;
+    }
+
+    public int getCenter(){
+        for (int i = 0; i<clusterGroup.size(); i++){
+            for (int j=0; j<clusterGroup.get(i).size(); j++) {
+                if(clusterGroup.get(i).get(j).equals(data.get((data.size()-1)))){
+
+                    return i;
+
+                }
+            }
+        }
+        return -1;
+    }
 
 
 
---
+++ src/test/java/org/wso2/siddhi/debs2017/WSO2KmeansclusteringTestCase.java
@@ -0,0 +1,67 @@
+package org.wso2.siddhi.debs2017;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.wso2.siddhi.debs2017.input.SparQLProcessor;
+import org.wso2.siddhi.debs2017.kmeans.WSO2KmeansClustering;
+
+import java.util.ArrayList;
+
+/*
+* Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+public class WSO2KmeansclusteringTestCase {
+    private static final Logger log = Logger.getLogger(WSO2KmeansClustering.class);
+
+
+    @Before
+    public void init() {
+
+    }
+
+    @org.junit.Test
+    public void Test1() throws InterruptedException {
+        log.info("WSO2KmeansClustering TestCase1");
+        ArrayList<Double> input = new ArrayList<>();
+        input.add(7.0);
+        input.add(8.0);
+        input.add(10.0);
+        input.add(1.0);
+        input.add(7.0);
+        input.add(9.0);
+        input.add(2.0);
+        WSO2KmeansClustering test =new WSO2KmeansClustering(3, 10, input);
+        log.info("Test case 1 result : "+test.getCenter());
+        Assert.assertEquals(test.getCenter(), 0);
+
+    }
+
+    @org.junit.Test
+    public void Test2() throws InterruptedException {
+        log.info("WSO2KmeansClustering TestCase2");
+        ArrayList<Double> input = new ArrayList<>();
+        input.add(1.0);
+        input.add(3.0);
+        input.add(2.0);
+        input.add(4.0);
+        input.add(5.0);
+        input.add(6.0);
+        WSO2KmeansClustering test =new WSO2KmeansClustering(2, 10, input);
+        log.info("Test case 2 result : "+test.getCenter());
+        Assert.assertEquals(test.getCenter(), 1);
+
+    }
+}
