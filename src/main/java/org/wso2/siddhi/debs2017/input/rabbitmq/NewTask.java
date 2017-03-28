062556e201466ce7f820988bb52164804cbcfd52
nihlaakram <nihlaakram@yahoo.com>
2017-03-28 11:07:05 +0530

    Change in number of messages

--- src/main/java/org/wso2/siddhi/debs2017/input/rabbitmq/NewTask.java
+++ src/main/java/org/wso2/siddhi/debs2017/input/rabbitmq/NewTask.java
@@ -71,7 +71,7 @@ public class NewTask {
                         if(dataInLine.contains("----")){
 
                             if(data.length()>100){
-                                for(int i =0; i<100; i++){
+                                for(int i =0; i<1; i++){
 
                                     count++;
                                     System.out.println(count);
